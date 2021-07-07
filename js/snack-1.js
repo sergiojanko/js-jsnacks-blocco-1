231e45cece6d3ee87cf6453cc1114467d9f2650b
sergiojanko <janko.sergio1998@gmail.com>
2021-07-07 15:37:54 +0200

    snack-1

--- js/snack-1.js
+++ js/snack-1.js
@@ -1,3 +1,27 @@
-/* 
+// Inizializzazione variabili
 
-*/
+var sum = 0;
+
+//Input numeri e somma (for)
+
+/* for (var i = 1; i <= 5; i++) {
+    do {
+        var num = parseInt(prompt("Inserisci il " + i + "° numero"))        
+    } while (isNaN(num));
+    sum += num;
+} */
+
+// Input numeri e somma (while)
+
+var i = 1;
+while (i <= 5) {
+    do {
+        var num = parseInt(prompt("Inserisci il " + i + "° numero"))        
+    } while (isNaN(num));
+    sum += num;
+    i++;
+}
+
+// Output
+
+console.log(sum);
