4a17583f8f99eb96474ddc6aca53dbac59838e49
sergiojanko <janko.sergio1998@gmail.com>
2021-07-07 18:01:00 +0200

    snack-4-output

--- js/snack-4.js
+++ js/snack-4.js
@@ -3,6 +3,9 @@
 var firstList = [2, 20, 57, 30, 40, 50, 80, 96, 20];
 var secondList = [80, 25, 36, 52, 887];
 
+var list1Element = document.getElementById("snack-4-list1");
+var list2Element = document.getElementById("snack-4-list2");
+
 // Verifico lunghezza dei due array e aggiungo elementi random all'array più corto
 
 while (firstList.length !== secondList.length) {
@@ -17,3 +20,16 @@ while (firstList.length !== secondList.length) {
 
 console.table(firstList);
 console.table(secondList);
+
+var listOutput = "";
+for (var i = 0; i < firstList.length; i++) {
+    listOutput += "<li>" + firstList[i] + "</li>"
+}
+
+list1Element.innerHTML = listOutput;
+var listOutput = "";
+for (var i = 0; i < secondList.length; i++) {
+    listOutput += "<li>" + secondList[i] + "</li>"
+}
+
+list2Element.innerHTML = listOutput;
