85f5931f4a7c58a9dcf3dce825af522575ac9e1f
sergiojanko <janko.sergio1998@gmail.com>
2021-07-07 17:38:32 +0200

    snack-2-output

--- js/snack-2.js
+++ js/snack-2.js
@@ -1,6 +1,6 @@
 // Input numero
 var num;
-
+var textElement = document.getElementById("snack-2-output")
 do {
     num = parseInt(prompt("Inserisci un numero", "7"));
 } while (isNaN(num) || num < 0);
@@ -8,6 +8,8 @@ do {
 // Verifica pari/dispari e output
 if (num % 2 === 0) {
     console.log(num);
+    textElement.innerHTML = "Hai inserito un numero pari: " + num;
 } else {
     console.log(num + 1);
+    textElement.innerHTML = "Hai inserito un numero dispari, numero successivo: " + (num + 1);
 }
